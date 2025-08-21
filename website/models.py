b6bbaad20429d7a8e58924b27b0dcd9181c9d78f
hasibhasan-github <hasibhasan483@gmail.com>
2025-08-21 11:45:17 +0600

    @hasibhasan-github

--- website/models.py
+++ website/models.py
@@ -13,3 +13,16 @@ class User(db.Model, UserMixin):
     city = db.Column(db.String(10),  nullable=False)
     bloodgroup = db.Column(db.String(3),  nullable=False)
     lastdonationdate = db.Column(db.String(15),  nullable=False)
+
+
+class Hero(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(30), unique = True,  nullable=False)
+    password = db.Column(db.String(30),  nullable=False)
+    username = db.Column(db.String(30),  nullable=False)
+    weight = db.Column(db.String(2),  nullable=False)
+    phone = db.Column(db.String(11),  nullable=False)
+    userRole = db.Column(db.String(20),  nullable=False)
+    city = db.Column(db.String(10),  nullable=False)
+    bloodgroup = db.Column(db.String(3),  nullable=False)
+    lastdonationdate = db.Column(db.String(15),  nullable=False)
