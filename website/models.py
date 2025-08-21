c1efcdf2e510b3655b1883a426c010ed6b3c9f0c
hasibhasan-github <hasibhasan483@gmail.com>
2025-08-21 16:30:01 +0600

    @hasibhasan-github

--- website/models.py
+++ website/models.py
@@ -27,3 +27,14 @@ class Fighter(db.Model, UserMixin):
     bloodgroup = db.Column(db.String(3),  nullable=False)
     hospital = db.Column(db.String(15),  nullable=False)
 
+class Hero(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(30), unique = True,  nullable=False)
+    password = db.Column(db.String(30),  nullable=False)
+    username = db.Column(db.String(30),  nullable=False)
+    weight = db.Column(db.String(2),  nullable=False)
+    phone = db.Column(db.String(11),  nullable=False)
+    userRole = db.Column(db.String(20),  nullable=False)
+    city = db.Column(db.String(10),  nullable=False)
+    bloodgroup = db.Column(db.String(3),  nullable=False)
+    ratings = db.Column(db.String(1), nullable=False, default="4")
