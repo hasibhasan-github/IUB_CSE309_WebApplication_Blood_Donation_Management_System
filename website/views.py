963a3be0077756a8297a1a4ba04209f88600fe84
hasibhasan-github <hasibhasan483@gmail.com>
2025-08-22 21:10:10 +0600

    @hasibhasan-github

--- website/views.py
+++ website/views.py
@@ -103,6 +103,7 @@ def Ratings():
 @views.route('/Hdashboard')
 @login_required
 def Hdashboard():
+    total_users = Hero.query.count()
     return render_template("HeroDashboard.html", user = current_user)
 
 
